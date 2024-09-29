7b91263bc1554b8c6a87dade4b777b3f1c8d3a54
vpolkampally0125 <vpolkampally0125@gmail.com>
2024-09-28 18:48:14 -0700

    Complete WebSocket SetUp

--- server/app.ts
+++ server/app.ts
@@ -1,70 +1,38 @@
-import express, { Request, Response } from 'express';
-import { createServer } from 'http';
 import WebSocket, { WebSocketServer } from 'ws';
-import request from 'request';
-
-// Express app and HTTP server
-const app = express();
-const server = createServer(app);
-
-// WebSocket server wrapped around HTTP server
-const wss = new WebSocketServer({ server });
 
 
-wss.on('connection', function connection(ws: WebSocket) {
-    ws.send("welcome")
-    ws.on('message', function incoming(message: WebSocket.RawData) {
-        console.log(message)
-    })
-})
+const wss = new WebSocketServer({ port: 8080 }); 
 
-const API_KEY = '';
-const SYMBOL = 'IBM';
-const INTERVAL = '5min';
-const ALPHA_VANTAGE_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${SYMBOL}&interval=${INTERVAL}&apikey=${API_KEY}`;
+wss.on('connection', (wsClient: WebSocket) => {
+    console.log('UI client connected');
+
+    wsClient.on('message', (message: WebSocket.RawData) => {
+        console.log(`Received message from UI client: ${message}`);
+    });
+});
 
 
-let lastData = ''
+var ws = new WebSocket('wss://api.tiingo.com/fx');
 
-function pollData() {
-    request.get({
-        url: ALPHA_VANTAGE_URL,
-        json: true,
-        headers: {'User-Agent': 'request'}
-      }, (err, res, data) => {
-        if (err) {
-          console.log('Error:', err);
-        } else if (res.statusCode !== 200) {
-          console.log('Status:', res.statusCode);
-        } else {
-          const timeSeries = data["Time Series (5min)"]
+var subscribe = {
+    'eventName':'subscribe',
+    'authorization':'790499a697516487709e9c0d27ff52e85d0c85ce',
+    'eventData': {
+        'thresholdLevel': 2
+    }
+}
+ws.on('open', function open() {
+    ws.send(JSON.stringify(subscribe));
+    console.log("Connected!")
+});
 
-            if (timeSeries) {
-                const latestData = timeSeries[Object.keys(timeSeries)[0]];
+ws.on('message', function(data: WebSocket.RawData, flags) {
+    console.log(data)
 
-                if (JSON.stringify(latestData) !== JSON.stringify(lastData)) {
-                    lastData = latestData
-
-                    wss.clients?.forEach(client => {
-                        if (client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify({ stockData: timeSeries }));
-                        }
-                    });
-                } else{
-                    console.log('no new data')
-                }  
-            }
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data); 
         }
     });
-}
-
-setInterval(pollData, 60000); 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
 });
-
-
-server.listen(8080, () => {
-    console.log('Server listening on port 8080');
-});
+   
